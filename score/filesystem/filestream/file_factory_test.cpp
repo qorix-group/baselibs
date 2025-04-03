5d19bb4640c701f582df5ceac0c3650087ade6bf
Markus Hosch <markus.hosch@bmw.de>
2025-04-03 11:29:08 +0200

    Fix UTs for QNX
    Since the unit tests on QNX aren't run with TEST_TMPDIR set and /tmp
    is not appropriate due to its missing rename support, we use a
    temporary directory on persistency for the tests, which anyway
    makes sense since this is where the renames will be used in 99% of
    the cases.
    
    Issue: SWP-168452
    GIT_ORIGIN_SPP_REV_ID: f268c2b98e81487dc7c70eea6deb476b9f1e5886

--- score/filesystem/filestream/file_factory_test.cpp
+++ score/filesystem/filestream/file_factory_test.cpp
@@ -15,6 +15,9 @@
 
 #include <gtest/gtest.h>
 
+#include <ftw.h>
+
+#include <cstdio>
 #include <iostream>
 
 namespace score
@@ -29,8 +32,20 @@ using namespace ::testing;
 class FileFactoryTest : public ::testing::Test
 {
   public:
-    FileFactoryTest() : unit_{}, test_tmpdir_{}
+    FileFactoryTest() : unit_{}, test_tmpdir_{}, remove_temp_{false}
     {
+        InitializeTempDir();
+    }
+
+    ~FileFactoryTest() override
+    {
+        RemoveTempContent();
+    }
+
+  protected:
+    void InitializeTempDir()
+    {
+        // First, check whether we have TEST_TMPDIR (which is set by Bazel)
         auto* env_tmpdir = std::getenv("TEST_TMPDIR");
         if (env_tmpdir != nullptr)
         {
@@ -38,13 +53,62 @@ class FileFactoryTest : public ::testing::Test
         }
         else
         {
+            // Otherwise, check whether we might be on a target
+            //
+            // This is a poor-man's test whether we're being run on real hardware
+            // If /persistent happens to exist anywhere else, we just assume that reusing this directory is also ok in
+            // case we're allowed to create a directory here.
+            //
+            // We're using pure POSIX here since the stdc++ filesystem lib doesn't seem to exist on the target toolchain
+            // and we're testing our filesystem library here which makes it brittle to use other parts of itself to test
+            // it.
+            struct ::stat statbuf;
+            if (::stat("/persistent", &statbuf) == 0)  // If "/persistent" exists...
+            {
+                if (statbuf.st_mode & S_IFDIR)  // ...and is a directory
+                {
+                    if (::stat("/persistent/tmp", &statbuf) != 0)  // ...and if "/persistent/tmp" does not exist
+                    {
+                        // ...then create it and also remove it during destruction
+                        remove_temp_ = true;
+                        if (::mkdir("/persistent/tmp", 0777) != 0)
+                        {
+                            throw std::runtime_error("Couldn't create dir");
+                        }
+                    }
+                    test_tmpdir_ = "/persistent/tmp";
+                }
+                else
+                {
+                    throw std::runtime_error("/persistent is not a directory. What gives?!");
+                }
+            }
+        }
+
+        // As a last resort, just use /tmp
+        if (test_tmpdir_.Empty())
+        {
             test_tmpdir_ = "/tmp";
         }
     }
 
-  protected:
+    static int RemoveDirentry(const char* fname, const struct stat*, int, struct FTW*)
+    {
+        std::remove(fname);
+        return 0;
+    }
+
+    void RemoveTempContent() const
+    {
+        if (remove_temp_)
+        {
+            ::nftw(test_tmpdir_.Native().c_str(), RemoveDirentry, 64, FTW_DEPTH);
+        }
+    }
+
     FileFactory unit_;
     Path test_tmpdir_;
+    bool remove_temp_;
 };
 
 TEST_F(FileFactoryTest, OpensFile)
