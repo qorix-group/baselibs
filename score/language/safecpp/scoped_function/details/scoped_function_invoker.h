9f13ca2cc98a4fd46ad2eeaa85ec9aabcb592412
BMW Engineer <tu-oss-contributions-gh-public@list.bmw.com>
2025-05-15 09:35:03 +0200

    scoped_functions_invoker: fix coverage and bugfix function signature
    - add class for testing coverage
    - bugfix ActualInvoker:
      Without this change the returned function signature will always be missing "noexcept"
      which could differ from the desired signature.
      This bugfix includes noexcept in the function signature if the user requrests it.
    
    Issue: SWP-187874
    GIT_ORIGIN_SPP_REV_ID: 2fa1e5740aa92ed1b103dda6014f4a6960e43e12

--- score/language/safecpp/scoped_function/details/scoped_function_invoker.h
+++ score/language/safecpp/scoped_function/details/scoped_function_invoker.h
@@ -33,7 +33,7 @@ class ScopedFunctionInvoker<NoExcept, ReturnType(Args...)>
     template <class ScopedFunction>
     [[nodiscard]] static CallableType<ScopedFunction> ActualInvoker() noexcept
     {
-        return [](ScopedFunction& base_scoped_function, Args... args) -> ReturnType {
+        return [](ScopedFunction& base_scoped_function, Args... args) noexcept(NoExcept) -> ReturnType {
             if (base_scoped_function.scope_state_ == nullptr || base_scoped_function.callable_ == nullptr)
             {
                 return {};
