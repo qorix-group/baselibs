e0185e2d972cd52e32f1f503b23fdd4f3ad574be
BMW Engineer <tu-oss-contributions-gh-public@list.bmw.com>
2025-08-06 16:41:49 +0200

    Daemon_folder : update structure + copybara
    Update folder structure
    
    Issue: SWP-205246
    GIT_ORIGIN_SPP_REV_ID: 1e06ba29e263ae27e513d3fb32393cb1c5a99e44

--- score/analysis/tracing/common/daemon_communication_messages.cpp
+++
@@ -1,8 +0,0 @@
-///
-/// @file daemon_communication_messages.h
-/// @copyright Copyright (C) 2023, Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
-///
-/// @brief Generic Trace API communication message IDs source file
-///
-
-#include "score/analysis/tracing/common/daemon_communication_messages.h"
