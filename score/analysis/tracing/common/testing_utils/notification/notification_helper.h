ea1860a57bc5df4ce6ee52c2ac93e8dc36b319a0
BMW Engineer <tu-oss-contributions-gh-public@list.bmw.com>
2025-08-13 15:26:22 +0200

    Gtl : remove sleep_for with promise notifier class
    Update unit tests to remove sleep_for
    
    Issue: SWP-201823
    GIT_ORIGIN_SPP_REV_ID: 274d90219f0b32d6409048fb0047194d2e68e421

--- score/analysis/tracing/common/testing_utils/notification/notification_helper.h
+++ score/analysis/tracing/common/testing_utils/notification/notification_helper.h
@@ -3,6 +3,8 @@
 
 #include <chrono>
 #include <condition_variable>
+#include <future>
+#include <memory>
 #include <mutex>
 
 namespace score
@@ -54,8 +56,95 @@ class NotificationHelper
     bool notified_;
 };
 
+class PromiseNotifier
+{
+  public:
+    PromiseNotifier() : promise_(std::make_shared<std::promise<void>>()) {}
+
+    void Notify()
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (promise_)
+        {
+            promise_->set_value();  // unblocks future.get()
+        }
+    }
+
+    void WaitForNotification()
+    {
+        auto future = GetFuture();
+        future.get();  // Blocks thread until promise is fulfilled. i.e, until Notify() is called
+    }
+
+    template <class Rep, class Period>
+    bool WaitForNotificationWithTimeout(const std::chrono::duration<Rep, Period>& timeout_duration)
+    {
+        auto future = GetFuture();
+        if (future.wait_for(timeout_duration) == std::future_status::ready)
+        {
+            future.get();
+            return true;
+        }
+        return false;  // Timeout occurred
+    }
+
+    void Reset()
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        promise_.reset();
+        promise_ = std::make_shared<std::promise<void>>();
+    }
+
+  private:
+    std::shared_future<void> GetFuture()
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        return promise_->get_future().share();
+    }
+
+    std::mutex mutex_;
+    std::shared_ptr<std::promise<void>> promise_;
+};
+
 }  // namespace tracing
 }  // namespace analysis
 }  // namespace score
 
 #endif /* SCORE_ANALYSIS_TRACING_COMMON_TESTING_UTILS_NOTIFICATION_HELPER_H */
+
+#include <gmock/gmock.h>
+
+// Simple ACTION_P for gMock to call Notify on a Notification object
+ACTION_P(CallNotify, notification)
+{
+    notification->Notify();
+}
+
+// Usage example in a test flow:
+//
+// #include "score/analysis/tracing/daemon/code/plugin/perf/notification.h"
+// #include <gmock/gmock.h>
+// #include <gtest/gtest.h>
+// #include <thread>
+//
+// using score::analysis::tracing::Notification;
+//
+// class MyMock {
+//  public:
+//   MOCK_METHOD(void, DoAsync, ());
+// };
+//
+// TEST(MyTest, NotifiesOnAsyncCall) {
+//   PromiseNotifier notification;
+//   MyMock mock;
+//   EXPECT_CALL(mock, DoAsync()).WillOnce(Notify(&notification));
+//
+//   std::thread t([&](){
+//     mock.DoAsync();
+//   });
+//
+//   notification.WaitForNotification();
+//   t.join();
+// }
+//
+// This test will pass if DoAsync() is called and notification.Notify() is triggered.
