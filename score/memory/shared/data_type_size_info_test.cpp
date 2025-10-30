0642f3efed3bf92a1cb0b78cef73c235d6744864
Brendan Emery <brendan.be.emery@bmw.de>
2025-10-30 17:14:59 +0100

    mw/com: Move DataTypeSizeInfo to lib/memory/shared
    GIT_ORIGIN_SPP_REV_ID: 9c7712d45881da9ca302e5b3bca94d6b8408aad1

---
+++ score/memory/shared/data_type_size_info_test.cpp
@@ -0,0 +1,67 @@
+/********************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+#include "score/memory/shared/data_type_size_info.h"
+
+#include <gtest/gtest.h>
+
+#include <cstddef>
+
+namespace score::memory::shared
+{
+namespace
+{
+
+constexpr std::size_t kDummySize{10U};
+constexpr std::size_t kDummyAlignment{20U};
+
+TEST(DataTypeSizeInfoEqualityTest, ObjectsWithSameSizeAndAlignmentCompareTrue)
+{
+    // Given two DataTypeSizeInfo objects with the same size and alignment
+    DataTypeSizeInfo unit{kDummySize, kDummyAlignment};
+    DataTypeSizeInfo unit2{kDummySize, kDummyAlignment};
+
+    // When comparing the two objects
+    const auto compare_result = unit == unit2;
+
+    // Then the result should be true
+    EXPECT_TRUE(compare_result);
+}
+
+TEST(DataTypeSizeInfoEqualityTest, ObjectsWithDifferentSizeCompareFalse)
+{
+    // Given two DataTypeSizeInfo objects with the different sizes
+    DataTypeSizeInfo unit{kDummySize, kDummyAlignment};
+    DataTypeSizeInfo unit2{kDummySize + 1U, kDummyAlignment};
+
+    // When comparing the two objects
+    const auto compare_result = unit == unit2;
+
+    // Then the result should be false
+    EXPECT_FALSE(compare_result);
+}
+
+TEST(DataTypeSizeInfoEqualityTest, ObjectsWithDifferentAlignemtCompareFalse)
+{
+    // Given two DataTypeSizeInfo objects with the different alignments
+    DataTypeSizeInfo unit{kDummySize, kDummyAlignment};
+    DataTypeSizeInfo unit2{kDummySize, kDummyAlignment + 1U};
+
+    // When comparing the two objects
+    const auto compare_result = unit == unit2;
+
+    // Then the result should be false
+    EXPECT_FALSE(compare_result);
+}
+
+}  // namespace
+}  // namespace score::memory::shared
