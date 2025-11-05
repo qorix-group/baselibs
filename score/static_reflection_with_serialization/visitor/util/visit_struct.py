432b68c89a5a34eb79145630915bcac5e4b208d3
BMW Engineer <tu-oss-contributions-gh-public@list.bmw.com>
2025-11-05 17:04:28 +0100

    Update visit_struct.py script
    - generate new visit_as_struct.h file using the visit_struct.py script
    - copy the generated macros to the original visit_as_struct.h file
    
    GIT_ORIGIN_SPP_REV_ID: f8ce5dfb7f79aaf62d7f71167d5606b9aa63947f

--- score/static_reflection_with_serialization/visitor/util/visit_struct.py
+++ score/static_reflection_with_serialization/visitor/util/visit_struct.py
@@ -1,26 +1,51 @@
 #!/usr/bin/env python3
 
-N = 50
+N = 64
 
 for x in range(N, 0, -1):
     strange = [str(t) for t in range(1, x + 1)]
     print("// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)")
+    print("// Macros of Field names and can't use parenthesis.")
+    print("// coverity[autosar_cpp14_a16_0_1_violation]")
+    print("// coverity[autosar_cpp14_m16_0_6_violation]")
+    print("// coverity[autosar_cpp14_m16_3_1_violation]")
     print("#define STRUCT_VISITABLE{}(S, F{}) \\".format(x, ", F".join(strange)))
-    print("    STRUCT_VISITABLE_START(S) \\")
-    print("    STRUCT_VISITABLE_FIELDNAMES(#F{}) \\".format(", #F".join(strange)))
-    print("    STRUCT_VISITABLE_FIELDS(s.F{}) \\".format(", s.F".join(strange)))
-    print("    STRUCT_VISITABLE_END(S)")
+    print("    STRUCT_VISITABLE_FULL_DEFINITION(S, \\")
+    print("    ::bmw::common::visitor::detail::pack_values(#F{}), s.F{}) \\".format(", #F".join(strange), ", s.F".join(strange)))
     print("")
 
 strange = [str(t) for t in range(1, N + 1)]
 print("// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)")
+print("// Macros for pass number of Arguments.")
+print("// Macros are used to pass length of args and can't use parenthesis")
+print("// coverity[autosar_cpp14_a16_0_1_violation]")
+print("// coverity[autosar_cpp14_m16_0_6_violation]")
 print("#define GET_NTH_ARG(_{}, N, ...) N".format(", _".join(strange)))
+
 print("// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)")
+print("// Macros for counting number of args.")
+print("// Macros are used to pass count args length and can't use parenthesis")
+print("// coverity[autosar_cpp14_a16_0_1_violation]")
+print("// coverity[autosar_cpp14_m16_0_6_violation]")
 print("#define COUNT_VARARGS(...) GET_NTH_ARG(__VA_ARGS__, {}, 0)".format(", ".join(strange[::-1])))
+
 print("// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)")
+print("// Macros for Concatenation.")
+print("// Macros are used to concatenate names and can't use parenthesis")
+print("// coverity[autosar_cpp14_a16_0_1_violation]")
+print("// coverity[autosar_cpp14_m16_0_6_violation]")
 print("#define DDAD_CONCATENATE2(A, B) A##B")
+
 print("// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)")
+print("// Macros for Concatenation.")
+print("// Macros are used to concatenate names and can't use parenthesis")
+print("// coverity[autosar_cpp14_a16_0_1_violation]")
+print("// coverity[autosar_cpp14_m16_0_6_violation]")
 print("#define DDAD_CONCATENATE(A, B) DDAD_CONCATENATE2(A, B)")
 print("")
 print("// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)")
+print("// Macros for Concatenation.")
+print("// Macros are used to concatenate names and can't use parenthesis")
+print("// coverity[autosar_cpp14_a16_0_1_violation]")
+print("// coverity[autosar_cpp14_m16_0_6_violation]")
 print("#define STRUCT_VISITABLE(S, ...) DDAD_CONCATENATE(STRUCT_VISITABLE, COUNT_VARARGS(__VA_ARGS__))(S, __VA_ARGS__)")
