912f957722cae0b20701ef3d748c64b4a6005813
BMW Engineer <tu-oss-contributions-gh-public@list.bmw.com>
2025-12-08 14:16:32 +0000

    fixing ut coverage
    (cherry picked from commit b79d04d2baf2fee2723f4f3037a438083126da88)
    Cherry-picking to branch:
    autocherry_master_moeb_fix_line_coverage_14215
    
    GIT_ORIGIN_SPP_REV_ID: 2f015f1b149ee9eba3429236d4cca35abd8e81db

--- score/analysis/tracing/common/shared_list/shared_list.h
+++ score/analysis/tracing/common/shared_list/shared_list.h
@@ -141,7 +141,7 @@ class alignas(std::max_align_t) List
         // DeallocateNode won't be called so this condition always evaluate to true.
         if (node != nullptr)  // LCOV_EXCL_BR_LINE not testable see comment above.
         {
-            if (flexible_allocator_)
+            if (flexible_allocator_)  // LCOV_EXCL_BR_LINE not testable see comment above.
             {
                 score::cpp::ignore = flexible_allocator_->Deallocate(static_cast<void*>(node), sizeof(Node));
             }
