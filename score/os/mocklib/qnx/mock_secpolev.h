efbb40543ed2efe73b64e920cdc02f66acfbd0fe
BMW Engineer <tu-oss-contributions-gh-public@list.bmw.com>
2026-01-13 16:32:13 +0530

    Fix or remove abstract mock provided to clients - mock_secpolev.h
    GIT_ORIGIN_SPP_REV_ID: 5108d3a037f3b304025da4a15e794860faa307ed

--- score/os/mocklib/qnx/mock_secpolev.h
+++ score/os/mocklib/qnx/mock_secpolev.h
@@ -41,6 +41,7 @@ class MockSecpolEv : public SecpolEv
                 (unsigned* flags),
                 (const, noexcept, override));
     MOCK_METHOD((score::cpp::expected_blank<score::os::Error>), flush, (), (const, noexcept, override));
+    MOCK_METHOD(uint64_t, get_dropped_event_count, (), (const, noexcept, override));
     MOCK_METHOD((score::cpp::expected<pid_t, score::os::Error>), get_pid, (), (const, noexcept, override));
     MOCK_METHOD((score::cpp::expected<const char*, score::os::Error>), get_process_name, (), (const, noexcept, override));
     MOCK_METHOD((score::cpp::expected<const char*, score::os::Error>), get_process_type, (), (const, noexcept, override));
