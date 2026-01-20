92c327a7d16b46920f24c6f6374b30d57c7ccf8b
Florian Bäuerle <florian.baeuerle@bmw.de>
2026-01-20 09:45:05 +0100

    score::cpp::jthread: ensure correct attribution of llvm-libcxx code
    The jthread implementation is based on llvm's libcxx, so add the license
    text and a note about the origin of the code.

--- score/language/futurecpp/include/score/private/thread/thread_id.hpp
+++ score/language/futurecpp/include/score/private/thread/thread_id.hpp
@@ -6,25 +6,12 @@
 //
 //===----------------------------------------------------------------------===//
 
-/********************************************************************************
- * Copyright (c) 2021 Contributors to the Eclipse Foundation
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Apache License Version 2.0 which is available at
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * SPDX-License-Identifier: Apache-2.0
- ********************************************************************************/
-
 ///
 /// \file
 /// \copyright Copyright (c) 2021 Contributors to the Eclipse Foundation
 ///
 /// The implementation is based on \c std::thread from https://github.com/llvm/llvm-project/tree/main/libcxx with
-/// modifications listed in \c LICENSE.txt.
+/// modifications listed in \c NOTICE.
 ///
 
 // IWYU pragma: private
