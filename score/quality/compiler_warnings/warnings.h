5109e88e2a4b54b1b1a39e1ee8bc669843253488
rutikakolkar <rutik.akolkar@bti.bmwgroup.com>
2026-03-03 18:22:56 +0530

    safecpp/string_view: fix clang-tidy warning
    GIT_ORIGIN_SPP_REV_ID: 2eb09747a0de9fa66dbd83247fc9cfd5e379bb88

--- score/quality/compiler_warnings/warnings.h
+++ score/quality/compiler_warnings/warnings.h
@@ -15,10 +15,12 @@
  * Extracted from https://www.fluentcpp.com/2019/08/30/how-to-disable-a-warning-in-cpp/
  */
 #if defined(__GNUC__) || defined(__clang__)
+// NOLINTBEGIN(score-banned-preprocessor-directives) : required due to false positive compiler warning
 #define DO_PRAGMA(X) _Pragma(#X)
 #define DISABLE_WARNING_PUSH DO_PRAGMA(GCC diagnostic push)
 #define DISABLE_WARNING_POP DO_PRAGMA(GCC diagnostic pop)
 #define DISABLE_WARNING(warningName) DO_PRAGMA(GCC diagnostic ignored #warningName)
+// NOLINTEND(score-banned-preprocessor-directives)
 
 // clang-format off
 #define DISABLE_WARNING_SUGGEST_OVERRIDE                        DISABLE_WARNING(-Wsuggest-override)
