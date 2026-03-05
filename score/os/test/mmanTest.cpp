828325a80eecd055ef427749df3401aae33dc398
BMW Engineer <tu-oss-contributions-gh-public@list.bmw.com>
2026-03-05 06:23:17 +0100

    Removed ifdef for qnx8
    GIT_ORIGIN_SPP_REV_ID: 4753a088217f04ef5f5103202d5a9cc34f27eece

--- score/os/test/mmanTest.cpp
+++ score/os/test/mmanTest.cpp
@@ -176,12 +176,7 @@ TEST(mmap, GetInfoInvalidFD)
     auto info_result = score::os::Mman::instance().posix_typed_mem_get_info(invalid_fd, &info);
 
     EXPECT_FALSE(info_result.has_value());
-    // TODO: Ticket-253097
-#if defined(__QNX__) && __QNX__ >= 800 && defined(__aarch64__)
-    EXPECT_EQ(info_result.error(), Error::Code::kNoSuchProcess);
-#else
     EXPECT_EQ(info_result.error(), Error::Code::kNoSuchFileOrDirectory);
-#endif
 }
 
 TEST(mmap, OpenTypedMemory)
