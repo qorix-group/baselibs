27dffb6f9e988f70a84e51e79cc2233525106ac7
Raghavendra Maddikery <Raghavendra.Maddikery@bmw.de>
2026-03-20 17:21:31 +0100

    Adds remote DLT backend plugin registrant
    GIT_ORIGIN_SPP_REV_ID: 103a67c18ba94ee52cb7eff7cbe1fcada56a1833

---
+++ score/mw/log/backend/remote_registrant_test.cpp
@@ -0,0 +1,41 @@
+/********************************************************************************
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+#include "score/mw/log/backend_table.h"
+
+#include "gtest/gtest.h"
+
+namespace score
+{
+namespace mw
+{
+namespace log
+{
+namespace detail
+{
+namespace
+{
+
+TEST(RemoteRegistrantTest, RemoteBackendIsRegisteredAfterStaticInitialization)
+{
+    RecordProperty("Description",
+                   "The remote backend registrant shall register a creator for LogMode::kRemote during static init.");
+    RecordProperty("TestingTechnique", "Requirements-based test");
+
+    EXPECT_TRUE(IsBackendAvailable(LogMode::kRemote));
+}
+
+}  // namespace
+}  // namespace detail
+}  // namespace log
+}  // namespace mw
+}  // namespace score
