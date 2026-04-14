69bd8297dc5de97db3362d8ea1da634883e7c27a
Florian Bäuerle <florian.baeuerle@bmw.de>
2026-04-14 14:17:18 +0200

    result: add missing copyright headers

--- score/result/error_msg_mapping_test.cpp
+++ score/result/error_msg_mapping_test.cpp
@@ -1,3 +1,16 @@
+/********************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
 #include "score/result/error_msg_mapping.h"
 #include "score/result/dummy_error_code.h"
 
