1a15cce83fb54745cee9851f5097028d5199363b
Ankur Satle <Ankur.Satle@bti.bmwgroup.com>
2026-08-17 11:44:44 +0200

    Don't `std::decay_t` the return of the `Func` in `and_then` functions

--- score/concurrency/locked_ptr.h
+++ score/concurrency/locked_ptr.h
@@ -205,7 +205,7 @@ class LockedPtr
      */
     template <typename Func,
               typename = std::enable_if_t<std::is_invocable_v<Func, LockedPtr&>>,
-              typename Ret = std::decay_t<std::invoke_result_t<Func, LockedPtr&>>,
+              typename Ret = std::invoke_result_t<Func, LockedPtr&>,
               typename = std::enable_if_t<score::cpp::is_optional_v<Ret>>>
     [[nodiscard]] auto and_then(Func&& f) & -> Ret
     {
@@ -226,7 +226,7 @@ class LockedPtr
      */
     template <typename Func,
               typename = std::enable_if_t<std::is_invocable_v<Func, const LockedPtr&>>,
-              typename Ret = std::decay_t<std::invoke_result_t<Func, const LockedPtr&>>,
+              typename Ret = std::invoke_result_t<Func, const LockedPtr&>,
               typename = std::enable_if_t<score::cpp::is_optional_v<Ret>>>
     [[nodiscard]] auto and_then(Func&& f) const& -> Ret
     {
@@ -240,14 +240,14 @@ class LockedPtr
     /**
      * @brief Monadic bind. Applies a callable that returns a score::cpp::optional.
      *        If the pointer is null, short-circuits to score::cpp::nullopt without invoking the callable.
-        *        The callable receives this LockedPtr as an rvalue and may transfer lock ownership by moving from it.
-        * @tparam Func Callable type that accepts an rvalue LockedPtr and returns a score::cpp::optional specialization.
+     *        The callable receives this LockedPtr as an rvalue and may transfer lock ownership by moving from it.
+     * @tparam Func Callable type that accepts an rvalue LockedPtr and returns a score::cpp::optional specialization.
      * @param f The callable to apply.
      * @return The result of f(std::move(*this)) if non-null, score::cpp::nullopt otherwise.
      */
     template <typename Func,
               typename = std::enable_if_t<std::is_invocable_v<Func, LockedPtr>>,
-              typename Ret = std::decay_t<std::invoke_result_t<Func, LockedPtr>>,
+              typename Ret = std::invoke_result_t<Func, LockedPtr>,
               typename = std::enable_if_t<score::cpp::is_optional_v<Ret>>>
     [[nodiscard]] auto and_then(Func&& f) && -> Ret
     {
@@ -268,7 +268,7 @@ class LockedPtr
      */
     template <typename Func,
               typename = std::enable_if_t<std::is_invocable_v<Func, const LockedPtr&>>,
-              typename Ret = std::decay_t<std::invoke_result_t<Func, const LockedPtr&>>,
+              typename Ret = std::invoke_result_t<Func, const LockedPtr&>,
               typename = std::enable_if_t<score::cpp::is_optional_v<Ret>>>
     [[nodiscard]] auto and_then(Func&& f) const&& -> Ret
     {
